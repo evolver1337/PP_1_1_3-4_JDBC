e7de10c9898693ca070dd988508e65e6d8e51bdc
Vitalii Kondrashov <52565293+evolver1337@users.noreply.github.com>
2024-04-06 14:40:51 +0300

    util

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,16 +1,31 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
+import static jm.task.core.jdbc.util.Util.connection;
+
 public class UserDaoJDBCImpl implements UserDao {
+    private static Connection connection = Util.getConnection();
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS users (id BIGINT PRIMARY KEY AUTO_INCREMENT" +
+                    ", name VARCHAR(45)" +
+                    ", last_name VARCHAR(45)" +
+                    ", age INT)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
@@ -18,7 +33,15 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try (PreparedStatement pst = connection
+                            .prepareStatement("INSERT INTO users (name, " +
+                             "lastName, age) VALUES (?, ?, ?)")){
+            pst.setString(1, name);
+            pst.setString(2, lastName);
+            pst.setByte(3, age);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.net.URL;
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final String URL = "jdbc:mysql://localhost:3306/mydatabase";
+    private final String USERNAME = "root";
+    private final String PASSWORD = "root";
+
+    private static Connection connection = null;
+
+
+    public static Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
 }
